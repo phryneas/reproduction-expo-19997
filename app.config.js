39d2bea70442c9b0552992fb5a4409eba8e54ec4
Lenz Weber <mail@lenzw.de>
2022-11-14 08:47:36 +0000

    reproduction

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+console.log("fooooo");
+
+module.exports = {
   "expo": {
     "name": "test-app",
     "slug": "test-app",
@@ -24,10 +26,16 @@
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#FFFFFF"
-      }
+      },
+      "package": "testi.test.test"
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+      "eas": {
+        "projectId": "e04907a5-5409-4f46-8b39-f2ac062e175b"
+      }
     }
   }
 }
